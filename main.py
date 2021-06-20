b9542b99d88558baa775b3c99218f7f284388bea
PrashanthGdd <81332434+PrashanthGdd@users.noreply.github.com>
2021-06-21 03:23:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,145 @@
+from flask import Flask,render_template,request,redirect, url_for, session
+
+app=Flask(__name__)
+
+
+
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+from tensorflow.python.framework import ops
+
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import random
+import json
+import pickle
+
+with open("Users.json") as us:
+    m_users = json.load(us)
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+ops.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+def chat(userText):
+    results = model.predict([bag_of_words(userText, words)])
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+    return random.choice(responses)
+
+
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/login',methods=["POST","GET"])
+def login():
+    Username = request.form["username"]
+    password = request.form["password"]
+
+    for i in m_users['usrs']:
+        if i['username'] == Username and i['password'] == password:
+            return render_template("chatbot.html")
+            break
+    else:
+        return render_template("home.html")
+
+
+
+@app.route('/get', methods=['GET','POST'])
+def get_bot_response():
+    userText = request.args.get("msg")
+    return str(chat(userText))
+
+if __name__ == '__main__':
+    app.run(debug=True)
